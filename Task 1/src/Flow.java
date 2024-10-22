1f53aef61146a4b85dbcf197abb2da333b9c4ff6
niki03125 <Nikita.po@hotmail.com>
2024-10-22 18:59:39 +0200

    update

---
+++ Task 1/src/Flow.java
@@ -0,0 +1,16 @@
+public class Flow {
+
+    public void methodA(){
+        System.out.println("a");
+    }
+
+    public void methodB(){
+        System.out.println("b");
+    }
+    public void methodC(){
+        System.out.println("c");
+    }
+    public void methodD(){
+        System.out.println("d");
+    }
+}
---
+++ Task 1/src/Main.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Flow flow = new Flow();
+
+        flow.methodA();
+        flow.methodB();
+        flow.methodC();
+        flow.methodD();
+        
+
+    }
+}
