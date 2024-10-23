74216018b7604fb09428d9fd7cf04eb2ffe7c971
niki03125 <Nikita.po@hotmail.com>
2024-10-23 19:53:23 +0200

    update

---
+++ Task 3/src/Person.java
@@ -0,0 +1,19 @@
+public abstract class Person {
+
+    private String name;
+
+    public Person(String name){
+        this.name = name;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public void setName(){
+        this.name=name;
+    }
+
+    public abstract boolean addCourse(String course);
+
+}
